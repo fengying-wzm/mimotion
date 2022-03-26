b13fed8ff4b91f0bbaf5c6424f822c97627ba0bc
风影 <297083979@qq.com>
2022-03-26 15:55:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -158,12 +158,6 @@ def getBeijinTime():
             for user_mi, passwd_mi in zip(user_list, passwd_list):
                 msg_mi += main(user_mi,passwd_mi,min_1, max_1)
                 #print(msg_mi)
-            if a:
-               push('【小米运动步数修改】', msg_mi)
-               push_wx(msg_mi)
-               run(msg_mi)
-            else:
-               print("此次修改结果不推送")
     else:
         print("当前不是主人设定的提交步数时间或者主人设置了0步数呢，本次不提交")
         return
